18ba7ba3993adec57203a25170a426e000d1555e
Sepehr Vahedi <sepehr.vahedi@gmail.com>
2025-08-28 13:04:34 +0330

    refactor: apply self-encapsulated field pattern to Symbol class

--- phase2/src/main/java/MiniJava/semantic/symbol/Symbol.java
+++ phase2/src/main/java/MiniJava/semantic/symbol/Symbol.java
@@ -1,15 +1,45 @@
 package MiniJava.semantic.symbol;
 
 /**
- * Created by mohammad hosein on 6/28/2015.
+ * Symbol class with self-encapsulated fields
  */
-
 public class Symbol {
-    public SymbolType type;
-    public int address;
+    private SymbolType type;
+    private int address;
 
     public Symbol(SymbolType type, int address) {
+        setType(type);
+        setAddress(address);
+    }
+
+    // Self-encapsulated field accessors
+    public SymbolType getType() {
+        return type;
+    }
+
+    public void setType(SymbolType type) {
+        if (type == null) {
+            throw new IllegalArgumentException("Symbol type cannot be null");
+        }
         this.type = type;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public void setAddress(int address) {
+        if (address < 0) {
+            throw new IllegalArgumentException("Address cannot be negative");
+        }
         this.address = address;
+    }
+
+    public boolean isNumericType() {
+        return getType() == SymbolType.Int;
+    }
+
+    public boolean isBooleanType() {
+        return getType() == SymbolType.Bool;
     }
 }
